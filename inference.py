da7db07888c54014ddf4601f410d7c7233edcc98
David Corvoysier <david@huggingface.co>
2023-06-20 10:10:42 +0000

    Modified the inference to use a real dataset

--- inference.py
+++ inference.py
@@ -1,14 +1,44 @@
 import os
 import argparse
+import json
 import torch
+from datasets import load_dataset
+from transformers import pipeline
+from evaluate import evaluator
 from optimum.neuron import NeuronModelForSequenceClassification
+from optimum.exporters import TasksManager
 from transformers import AutoTokenizer, AutoConfig
+
+
+def read_model_id(model_dir):
+    with open(os.path.join(model_dir, 'config.json')) as f:
+        return json.load(f)['_name_or_path']
+
+
+def get_neuron_model_class(model_id):
+    # Infer task from the Hub model id
+    task = TasksManager.infer_task_from_model(model_id)
+    # Get AutoModel class for task
+    auto_model_class = TasksManager.get_model_class_for_task(task)
+    neuron_classes = [NeuronModelForSequenceClassification]
+    for neuron_class in neuron_classes:
+        if neuron_class.auto_model_class == auto_model_class:
+            return neuron_class, task
+    raise ValueError(f"{model_id} is associated to {task} which is not supported for Neuron (yet).")
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='distilbert-base-uncased-finetuned-sst-2-english',
                         help='The model to use for inference: can be a model ID, or the path to a local directory.')
+    parser.add_argument('--dataset', type=str, default='sst2',
+                        help='The dataset on which the model should be evaluated.')
+    parser.add_argument('--num-samples', type=int,
+                        help='The number of samples to use for evaluation.')
+    parser.add_argument('--metric', type=str, default='accuracy',
+                        help='The metric used to evaluate the model.')
+    parser.add_argument('--input-column', type=str, default='sentence',
+                        help='The name of the input column in the selected dataset.')
     parser.add_argument('--batch-size', type=int, default=1,
                         help='The static batch size of the Neuron model.')
     parser.add_argument('--seq-length', type=int, default=128,
@@ -18,29 +48,48 @@ def main():
     args = parser.parse_args()
 
     if os.path.exists(args.model):
-        # This is a model that has already been exported to Neuron, just load it with its tokenizer
-        model = NeuronModelForSequenceClassification.from_pretrained(args.model)
+        # Extract original model Hub name from the config (Too brittle ?)
+        model_id = read_model_id(args.model)
+        # Get the corresponding Neuron class
+        neuron_model_class, task = get_neuron_model_class(model_id)
+        model = neuron_model_class.from_pretrained(args.model)
         # Sanity checks
         assert args.batch_size == model.config.neuron_batch_size
         assert args.seq_length == model.config.neuron_sequence_length
     else:
-        model = NeuronModelForSequenceClassification.from_pretrained(args.model, export=True, batch_size=1, sequence_length=args.seq_length)
+        # Get the Neuron class for the specified model_id
+        neuron_model_class, task = get_neuron_model_class(args.model)
+        model = neuron_model_class.from_pretrained(args.model,
+                                                   export=True,
+                                                   batch_size=args.batch_size,
+                                                   sequence_length=args.seq_length)
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     if args.save_dir:
         model.save_pretrained(os.path.join(args.save_dir))
 
-    # create embeddings for inputs
-    inputs = "To be, or not to be: that is the question: Whether it is nobler in the mind" \
-             " to suffer the slings and arrows of outrageous fortune, Or to take arms against a sea of troubles"
-    embeddings = tokenizer(inputs, return_tensors="pt")
-    # convert to tuple for neuron model
-    neuron_inputs = tuple(embeddings.values())
+    # Load validation dataset
+    eval_dataset = load_dataset(args.dataset, split="validation")
+    if args.num_samples:
+        eval_dataset = eval_dataset.select(range(args.num_samples))
 
-    # run prediction
-    with torch.no_grad():
-        logits = model(*neuron_inputs)[0]
-        cls_id = logits.argmax().item()
-        print(model.config.id2label[cls_id])
+    # Create evaluation pipeline
+    eval_pipe = pipeline(
+        task,
+        model=model,
+        tokenizer=tokenizer,
+        max_length=args.seq_length,
+        padding="max_length",
+        truncation=True
+    )
+
+    # Instantiate evaluator
+    task_evaluator = evaluator(task)
+    metric = task_evaluator.compute(model_or_pipeline=eval_pipe,
+                                    data=eval_dataset,
+                                    metric=args.metric,
+                                    input_column=args.input_column,
+                                    label_mapping=model.config.label2id)
+    print(metric)
 
 
 if __name__ == "__main__":
